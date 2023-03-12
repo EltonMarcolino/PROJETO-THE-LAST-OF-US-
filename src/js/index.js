b3934de506ad71bb5421629c3f215b673a8818c0
Elton Marcolino <118011758+EltonMarcolino@users.noreply.github.com>
2023-03-11 23:06:25 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,14 @@
+const botoesCarrosel = document.querySelectorAll('.botao'); //buscar os elementos botões.
+const imagens = document.querySelectorAll('.imagem');
+
+botoesCarrosel.forEach((botao, indice) => {
+    botao.addEventListener('click', () => {
+        const botaoSelecionado = document.querySelector('.selecionado'); // indetificar o click no botão.
+        botaoSelecionado.classList.remove('selecionado'); // desmarcar o botao selecionado.
+        botao.classList.add('selecionado');//marcar botão clicado.
+        const imagemAtiva = document.querySelector('.ativa');
+        imagemAtiva.classList.remove('ativa');// esconder a imagem ativa.
+        imagens[indice].classList.add('ativa');//fazer aparecer a imagem selecionado. 
+    }) 
+})
+
